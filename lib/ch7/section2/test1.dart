0826a0b4b5a1c473f4099f6402308b1785bd2117
wanna <sangsangcat@gmail.com>
2026-04-23 11:01:47 +0900

    플러터 day11 Provider - MultiProvider, ProxyProvider, 실습

--- lib/ch7/section2/test1.dart
+++ lib/ch7/section2/test1.dart
@@ -13,6 +13,24 @@ class Counter with ChangeNotifier {
   }
 }
 
+//provider 를 이용해 공개해야 하는 상태 데이터.. 다른 상태 데이터 참조해서..
+class Sum {
+  int _sum = 0;
+
+  int get sum => _sum;
+
+  void set sum(value) {
+    _sum = 0;
+    for (int i = 1; i <= value; i++) {
+      _sum += i;
+    }
+  }
+
+  Sum(Counter counter) {
+    sum = counter.count;
+  }
+}
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -21,8 +39,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('provider test')),
-        body: ChangeNotifierProvider<Counter>.value(
-          value: Counter(),
+        body: MultiProvider(
+          providers: [
+            ChangeNotifierProvider<Counter>.value(value: Counter()),
+            Provider<int>.value(value: 10),
+            Provider<String>.value(value: 'hello'),
+            //<내가 받아야하는 상태 타입, 내가 발행하는 상태 타입>
+            ProxyProvider<Counter, Sum>(
+              //model : 내가 참조해야 하는 다른 상태
+              //sum : 내가 이전에 발행했던 객체
+              update: (context, model, sum) {
+                if (sum != null) {
+                  sum.sum = model.count;
+                  return sum;
+                } else {
+                  return Sum(model);
+                }
+              },
+            ),
+          ],
           child: SubWidget(),
         ),
       ),
@@ -36,6 +71,11 @@ class SubWidget extends StatelessWidget {
     //상위에 선언된 provider 상태 획득...타입으로 식별해서..
     var counter = Provider.of<Counter>(context);
 
+    var int_data = Provider.of<int>(context);
+    var string_data = Provider.of<String>(context);
+
+    var sum = Provider.of<Sum>(context);
+
     return Column(
       children: [
         Text('provider counter : ${counter.count}'),
@@ -45,6 +85,9 @@ class SubWidget extends StatelessWidget {
           },
           child: Text('increment'),
         ),
+        Text('provider int : ${int_data}'),
+        Text('provider string : ${string_data}'),
+        Text('provider sum : ${sum.sum}'),
       ],
     );
   }
